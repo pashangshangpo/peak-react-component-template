449e5c9807f97fcde4cb6c12aad4248eeb6f1ffa
爬上上坡 <pashangshangpo@gmail.com>
2018-11-25 15:19:28 +0800

    新增 className 工具

--- common/index.js
+++ common/index.js
@@ -5,7 +5,31 @@
  */
 
 import React from 'react'
+import Classnames from 'classnames'
+
+const PREFIX = 'react'
 
 export const El = (...arg) => {
   return React.createElement(...arg)
+}
+
+/**
+ * @param {*} prefix
+ * @param {*} className
+ * @example
+ * C(
+ * 'button',
+ *   {
+ *     true: true,
+ *     false: false,
+ *     undefined: undefined,
+ *     null: null,
+ *     string: '123'
+ *   }
+ * )
+ *
+ * react-button true string
+ */
+export const C = (prefix, className) => {
+  return Classnames(`${PREFIX}-${prefix}`, className)
 }
